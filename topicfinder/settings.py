08da7e3075d9a0991b48543691d14d765963d5ba
chiwonisoruguva <chiwonisoruguva@gmail.com>
2026-05-12 22:51:55 +0200

    Change email backend to console for debugging

--- topicfinder/settings.py
+++ topicfinder/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
