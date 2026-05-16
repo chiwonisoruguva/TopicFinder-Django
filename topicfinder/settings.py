002691310807addee4c9d5ffdeb4bebd33953b3c
chiwonisoruguva <chiwonisoruguva@gmail.com>
2026-05-16 21:25:43 +0200

    Switch to port 465 SSL for SendGrid

--- topicfinder/settings.py
+++ topicfinder/settings.py
@@ -151,7 +151,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY', '')
